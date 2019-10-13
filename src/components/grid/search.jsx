ea12b13eb4b223f1cd0dce1cac9f93c84ef57a34
Izel Aciman <izelaciman@gmail.com>
2019-10-13 16:44:41 +0100

    refactor form.

--- src/components/grid/search.jsx
+++ src/components/grid/search.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 
 export default function Search(props) {
     return (
-        <form className="form-inline">
-            <div className="form-group mx-sm-3 mb-2">
-                <input type="text" onChange={props.searchHandler} className="form-control" id="search" placeholder="Search" />
-            </div>
-        </form>
+        <input type="text" onChange={props.searchHandler} className="form-control" id="search" placeholder="Search by name" />                
     ) 
 }
