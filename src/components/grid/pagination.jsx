2ef88e1f097c0cc930506e9491fb7bca2e61aac3
Izel Aciman <izelaciman@gmail.com>
2019-10-21 19:26:36 +0100

    Add next and previous button.

--- src/components/grid/pagination.jsx
+++ src/components/grid/pagination.jsx
@@ -18,12 +18,36 @@ export default class Pagination extends PureComponent {
             )
         })
     }
+    buildPrevious() {
+        if(this.props.currentPage > 1) {
+        return ( 
+            <li>
+                <button name='page' onClick={this.props.paginationHandler} value={this.props.currentPage-1} className="page-link" >Previous
+                </button>
+            </li>)
+        } else {
+            return []
+        }
+    }
+    buildNext() {
+        if(this.props.currentPage < this.calculateTotalPage()) {
+        return ( 
+            <li>
+                <button name='page' onClick={this.props.paginationHandler} value={this.props.currentPage+1} className="page-link" >Next
+                </button>
+            </li>)
+        } else {
+            return []
+        }
+    }
     render() {
         return (
          this.calculateTotalPage() > 1 &&
             <nav aria-label="Page navigation">
                 <ul className="pagination justify-content-center">
+                    {this.buildPrevious()}
                     {this.buildPageNumbers()}      
+                    {this.buildNext()}
                 </ul>
             </nav>
         )
